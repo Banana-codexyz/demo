7fb00a477976f77edc0fa2b109a1bccd39025802
Nguyen <nguyenkhoinguyen139@gmail.com>
2023-11-02 23:24:39 +0700

    testA

--- src/main/java/com/nguyenz/demo/controller/UserController.java
+++ src/main/java/com/nguyenz/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.nguyenz.demo.controller;
 
+import com.nguyenz.demo.serivce.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-    @GetMapping("/getCode1")
-    public String getCode1() {
-        return "Code 1";
+    @Autowired
+    private UserService userService;
+    @GetMapping("/testA")
+    public String testA() {
+        userService.testA();
+        return "testA";
     }
 
 }
--- src/main/java/com/nguyenz/demo/serivce/UserService.java
+++ src/main/java/com/nguyenz/demo/serivce/UserService.java
@@ -1,4 +1,10 @@
 package com.nguyenz.demo.serivce;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public void testA() {
+        System.out.println("Nguyen test A");
+    }
 }
